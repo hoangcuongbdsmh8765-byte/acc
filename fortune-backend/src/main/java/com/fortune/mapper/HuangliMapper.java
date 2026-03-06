9d488245cc92eb9e69cfe39d3235a57201964d15
code <hoangcuongbdsmh8765@gmail.com>
2026-03-06 15:45:32 +0800

    add-more-mappers

---
+++ fortune-backend/src/main/java/com/fortune/mapper/HuangliMapper.java
@@ -0,0 +1,9 @@
+package com.fortune.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fortune.entity.Huangli;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HuangliMapper extends BaseMapper<Huangli> {
+}
